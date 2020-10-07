d6b3eb25ce7a552650a1f9161b1ae7b0f6503c92
DMiyalu <dieudonnemiyalu6@gmail.com>
2020-10-07 16:36:08 +0100

    product card

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, SafeAreaView, TextInput } from 'react-native'
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { 
+    Text, 
+    View, 
+    StyleSheet, 
+    SafeAreaView, 
+    TextInput, 
+    ScrollView,
+    Image,
+} from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
 export class Home extends Component {
@@ -22,6 +30,41 @@ export class Home extends Component {
                             />
                         </View>
                     </View>
+                    <ScrollView
+                        scrollEventThrottle={20}
+                    >
+                        <View style={styles.mainSliders} >
+                            <Text style={styles.textTop} >
+                                Des légumes et des Viandes 100% Bio...
+                            </Text>
+                            <View style={styles.boxSliders} >
+                                <ScrollView>
+                                    <View style={styles.sliderItem} >
+                                        <View style={styles.imageBox} >
+                                            <Image 
+                                                source={require('../../assets/viande_pack.jpg')} 
+                                                style={styles.imageFile}
+                                            />
+                                        </View>
+                                        <View style={styles.imageTextDescription} >
+                                            <View>
+                                                <Text style={styles.imageDescription} >
+                                                    VIANDE HACHEE
+                                                </Text>
+                                            </View>
+                                            <View>
+                                                <Text style={styles.imagePrice} >
+                                                    5$ par kilos
+                                                </Text>
+                                            </View>  
+                                        </View>
+                                    </View>
+                                </ScrollView>
+                            </View>
+
+                        </View> 
+
+                    </ScrollView>
                 </View>
             </SafeAreaView>
         )
@@ -62,5 +105,49 @@ const styles = StyleSheet.create({
     textInput: {
         backgroundColor: "white",
         fontWeight: '700',
+    },
+    textTop: {
+        fontSize: 24,
+        fontWeight: '700',
+        paddingHorizontal: 20,
+        color: "rgba(0, 0, 0, 0.8)",
+    },
+    mainSliders: {
+        flex: 1,
+        backgroundColor: "white",
+        paddingTop: 10,
+    },
+    boxSliders: {
+        height: 130,
+        marginTop: 20,
+    },
+    sliderItem: {
+        height: 130,
+        width: 130,
+        marginLeft: 20,
+        borderWidth: 0.5,
+        borderColor: "#dddddd",
+    },
+    imageBox: {
+        flex: 2,
+    },
+    imageTextDescription: {
+        flex: 1,
+        paddingHorizontal: 10,
+        paddingTop: 10,
+    },
+    imageFile: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: "cover",
+    },
+    imageDescription: {
+        fontWeight: '700',
+        color: "rgba(0, 0, 0, 0.8) ",
+    },
+    imagePrice: {
+        fontWeight: '300',
+        color: "rgba(0, 0, 0, 0.4) ",
     }
 });
