8f9560c7d050abff6145e7b3c6f8d6a2ceea908a
DMiyalu <dieudonnemiyalu6@gmail.com>
2020-10-09 16:43:26 +0100

    call api_product success

--- screens/Saved/index.js
+++ screens/Saved/index.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, SafeAreaView } from 'react-native'
+import axios from 'axios'
+
+
+
+
 
 export class Saved extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            apiData: []
+        }
+    }
+
+    componentWillMount() {
+        axios.get('http://127.0.0.1:8080/product')
+        .then((response) => {
+            this.setState({ apiData: response.data })
+            console.log("Response :", response)
+            console.log("State: ", this.state.apiData);
+        })
+    }
+
+    renderProduct() {
+        return this.state.apiData.map((product) => <View style={{ flex: 1, justifyContent: "center", alignItems: "center", paddingHorizontal: 20, paddingVertical: 20, borderWidth: 1, borderRadius: 3, margin:10 }} >
+            <Text> {product.title} </Text>
+            <Text> {product.description} </Text>
+            <Text> {product.price} </Text>
+        </View>) 
+    }
+
+
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
+            <SafeAreaView style={{ flex: 1, backgroundColor: "white" }} >
+                <View style={{ flex: 1, backgroundColor: "#dede", height: 20, width: "100%" }} >
+                    <Text> List Favoris </Text>
+                    <View style={{ flex: 1 }} >
+                        {this.renderProduct()}
+                    </View>
+                </View>
+            </SafeAreaView>
         )
     }
 }
