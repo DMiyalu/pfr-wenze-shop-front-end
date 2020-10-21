474c5c41dfe504efef1a84e84329e5489a1b8277
DMiyalu <dieudonnemiyalu6@gmail.com>
2020-10-21 17:08:22 +0100

    reset root-reducers

--- Redux/root-reducer.js
+++ Redux/root-reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 import PanierReducer from './Panier/panier.reducers'
 import ProductReducer from './Product/product.reducers'
+import AllProduct from './AllProduct/allProduct.reducers'
 
 
 const rootReducer = combineReducers({
     panier: PanierReducer,
     product: ProductReducer,
+    allProduct: AllProduct,
 })
 
 export default rootReducer 
