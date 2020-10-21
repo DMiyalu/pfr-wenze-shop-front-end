2224deb5e68a9cb3a8d7c5ad757e9e142d1d2629
DMiyalu <dieudonnemiyalu6@gmail.com>
2020-10-21 18:16:06 +0100

    update initialData from redux

--- Redux/AllProduct/allProduct.reducers.js
+++ Redux/AllProduct/allProduct.reducers.js
@@ -5,25 +5,39 @@ const INITIAL_STATE = {
     allProduct: [
         {
             productID: 1,
-            title: "Initial title",
-            description: 'Initial words to describe this product.',
-            price: 45,
+            title: "Sandwich",
+            description: 'Sandwich au poulet mayo et du salade.',
+            price: 8,
             number: 1
         },
         {
             productID: 2,
-            title: "Initial title",
-            description: 'Initial words to describe this product.',
-            price: 45,
+            title: "Choco Cake",
+            description: 'Choco cake à base du lait de vache.',
+            price: 4,
             number: 1
         },
         {
             productID: 3,
-            title: "Initial title",
-            description: 'Initial words to describe this product.',
-            price: 45,
+            title: "Chocolate",
+            description: 'Jus chocolate, en crème et en poudre, à dilué sur 1 litre d\'eau.',
+            price: 19,
             number: 1
-        }
+        },
+        {
+            productID: 4,
+            title: "Choco Cake",
+            description: 'Choco cake à base du lait de vache.',
+            price: 20,
+            number: 1
+        },
+        {
+            productID: 5,
+            title: "Choco Cake",
+            description: 'Choco cake à base du lait de vache.',
+            price: 5,
+            number: 1
+        },
     ]
 }
 
