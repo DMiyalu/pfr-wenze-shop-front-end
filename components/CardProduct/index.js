1b2b2dc925f4788b468a104f37dfee9948b68c00
DMiyalu <dieudonnemiyalu6@gmail.com>
2020-10-22 10:57:36 +0100

    card product reset ui

--- components/CardProduct/index.js
+++ components/CardProduct/index.js
@@ -36,13 +36,27 @@ export default CardProduct
 const styles = StyleSheet.create({
     sliderItem: {
         height: 130,
-        width: 130,
-        marginLeft: 20,
+        width: 150,
+        marginHorizontal: 5,
+        paddingHorizontal: 5,
+        paddingTop: 5,
+        paddingBottom: 3,
         borderWidth: 0.5,
         borderColor: "#dddddd",
+        borderRadius: 5,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.20,
+        shadowRadius: 1.41,
+        elevation: 1,
     },
     imageBox: {
         flex: 2,
+        paddingHorizontal: 5,
+        width: 140,
     },
     imageTextDescription: {
         display: "flex",
